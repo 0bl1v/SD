e3fd3689287908feda84909c81a63ac0f96b6e1c
Obliv <leeliam856@gmail.com>
2025-10-30 16:40:55 +0300

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -72,7 +72,7 @@ function Library:CreateHub(hubName)
 	TitleLabel.Font = Enum.Font.Code
 	TitleLabel.Text = hubName
 	TitleLabel.TextColor3 = CONFIG.Colors.Text
-	TitleLabel.TextSize = 16
+	TitleLabel.TextSize = 14
 	TitleLabel.TextXAlignment = Enum.TextXAlignment.Left
 	TitleLabel.Parent = MainFrame
 	
@@ -87,7 +87,7 @@ function Library:CreateHub(hubName)
 	local MinimizeButton = Instance.new("TextLabel")
 	MinimizeButton.Name = "MinimizeButton"
 	MinimizeButton.BackgroundTransparency = 1
-	MinimizeButton.Position = UDim2.new(1, -45, 0, 9)
+	MinimizeButton.Position = UDim2.new(1, -55, 0, 9)
 	MinimizeButton.Size = UDim2.new(0, 20, 0, 20)
 	MinimizeButton.Font = Enum.Font.Code
 	MinimizeButton.Text = "-"
@@ -135,16 +135,13 @@ function Library:CreateHub(hubName)
 	
 	local CloseButton = Instance.new("TextLabel")
 	CloseButton.Name = "CloseButton"
-	CloseButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	CloseButton.BackgroundTransparency = 1
-	CloseButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-	CloseButton.BorderSizePixel = 0
-	CloseButton.Position = UDim2.new(1, -28, 0, 8)
+	CloseButton.Position = UDim2.new(1, -23, 0, 9)
 	CloseButton.Size = UDim2.new(0, 20, 0, 20)
 	CloseButton.Font = Enum.Font.Code
-	CloseButton.Text = "x"
+	CloseButton.Text = "×"
 	CloseButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-	CloseButton.TextSize = 16
+	CloseButton.TextSize = 20
 	CloseButton.Parent = MainFrame
 	
 	local CloseDetector = Instance.new("TextButton")
